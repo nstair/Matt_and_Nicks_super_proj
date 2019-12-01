9953554634acdd0f1d40b1751e285cfcc3409d0e
Matthew Wilson <mattwilson888@gmail.com>
2019-12-01 00:51:29 -0800

    Added file 'learningmaterial.txt' to keep track of useful tutorials or information.

---
+++ CNNs.py
@@ -0,0 +1,8 @@
+###The name “convolutional neural network” indicates that the network employs a mathematical operation called convolution. Convolution is a specialized kind of linear operation. Convolutional networks are simply neural networks that use convolution in place of general matrix multiplication in at least one of their layers. ~~ https://en.wikipedia.org/wiki/Convolutional_neural_network
+
+#This video is timestamped at the meaty descriptrion of what 'convolution' means in the context of image detection.
+# https://youtu.be/YRhxdVk_sIs?t=220
+
+#At about the 5:40 mark in the video the presenter explains that the Convolutional layer is determined the dot product of two 3x3 matrices which confused me as I've never seen and couldn't find any example or definition of the dot product of two matrices rather than vectors. 
+
+#For a sanity check I confirmed with a calculator that this dot product is simply the sum of each product of each corrosponding element in the two matrices.
